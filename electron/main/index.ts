50d10967a66e92cd1deaff7c22ee6895a4db573e
MoYiJiangNans <1545929126@qq.com>
2024-04-28 22:01:43 +0800

    ae页面功能实现

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -1,8 +1,10 @@
-import { app, BrowserWindow, shell, ipcMain } from 'electron'
-import { createRequire } from 'node:module'
-import { fileURLToPath } from 'node:url'
+import {app, BrowserWindow, ipcMain, shell} from 'electron'
+import {createRequire} from 'node:module'
+import {fileURLToPath} from 'node:url'
 import path from 'node:path'
 import os from 'node:os'
+import {readdirSync, statSync, readFileSync} from "fs";
+import {Parser} from 'xml2js';
 
 const require = createRequire(import.meta.url)
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
@@ -13,8 +15,8 @@ export const RENDERER_DIST = path.join(process.env.APP_ROOT, 'dist')
 export const VITE_DEV_SERVER_URL = process.env.VITE_DEV_SERVER_URL
 
 process.env.VITE_PUBLIC = VITE_DEV_SERVER_URL
-  ? path.join(process.env.APP_ROOT, 'public')
-  : RENDERER_DIST
+    ? path.join(process.env.APP_ROOT, 'public')
+    : RENDERER_DIST
 
 // Disable GPU Acceleration for Windows 7
 if (os.release().startsWith('6.1')) app.disableHardwareAcceleration()
@@ -23,8 +25,8 @@ if (os.release().startsWith('6.1')) app.disableHardwareAcceleration()
 if (process.platform === 'win32') app.setAppUserModelId(app.getName())
 
 if (!app.requestSingleInstanceLock()) {
-  app.quit()
-  process.exit(0)
+    app.quit()
+    process.exit(0)
 }
 
 let win: BrowserWindow | null = null
@@ -32,72 +34,110 @@ const preload = path.join(__dirname, '../preload/index.mjs')
 const indexHtml = path.join(RENDERER_DIST, 'index.html')
 
 async function createWindow() {
-  win = new BrowserWindow({
-    title: 'Main window',
-    icon: path.join(process.env.VITE_PUBLIC, 'favicon.ico'),
-    webPreferences: {
-      preload,
-    },
-  })
+    win = new BrowserWindow({
+        title: 'Main window',
+        icon: path.join(process.env.VITE_PUBLIC, 'favicon.ico'),
+        webPreferences: {
+            preload,
+        },
+    })
+    win.setMenu(null);
+    if (VITE_DEV_SERVER_URL) { // #298
+        await win.loadURL(VITE_DEV_SERVER_URL)
+        // Open devTool if the app is not packaged
+        win.webContents.openDevTools()
+    } else {
+        await win.loadFile(indexHtml)
+    }
 
-  if (VITE_DEV_SERVER_URL) { // #298
-    await win.loadURL(VITE_DEV_SERVER_URL)
-    // Open devTool if the app is not packaged
-    win.webContents.openDevTools()
-  } else {
-    await win.loadFile(indexHtml)
-  }
+    // Test actively push message to the Electron-Renderer
+    win.webContents.on('did-finish-load', () => {
+        win?.webContents.send('main-process-message', new Date().toLocaleString())
+    })
 
-  // Test actively push message to the Electron-Renderer
-  win.webContents.on('did-finish-load', () => {
-    win?.webContents.send('main-process-message', new Date().toLocaleString())
-  })
-
-  // Make all links open with the browser, not with the application
-  win.webContents.setWindowOpenHandler(({ url }) => {
-    if (url.startsWith('https:')) shell.openExternal(url)
-    return { action: 'deny' }
-  })
-  // win.webContents.on('will-navigate', (event, url) => { }) #344
+    // Make all links open with the browser, not with the application
+    win.webContents.setWindowOpenHandler(({url}) => {
+        if (url.startsWith('https:')) shell.openExternal(url)
+        return {action: 'deny'}
+    })
+    // win.webContents.on('will-navigate', (event, url) => { }) #344
 }
 
 app.whenReady().then(createWindow)
 
 app.on('window-all-closed', () => {
-  win = null
-  if (process.platform !== 'darwin') app.quit()
+    win = null
+    if (process.platform !== 'darwin') app.quit()
 })
 
 app.on('second-instance', () => {
-  if (win) {
-    // Focus on the main window if the user tried to open another
-    if (win.isMinimized()) win.restore()
-    win.focus()
-  }
+    if (win) {
+        // Focus on the main window if the user tried to open another
+        if (win.isMinimized()) win.restore()
+        win.focus()
+    }
 })
 
 app.on('activate', () => {
-  const allWindows = BrowserWindow.getAllWindows()
-  if (allWindows.length) {
-    allWindows[0].focus()
-  } else {
-    createWindow()
-  }
+    const allWindows = BrowserWindow.getAllWindows()
+    if (allWindows.length) {
+        allWindows[0].focus()
+    } else {
+        createWindow()
+    }
 })
 
 // New window example arg: new windows url
 ipcMain.handle('open-win', (_, arg) => {
-  const childWindow = new BrowserWindow({
-    webPreferences: {
-      preload,
-      nodeIntegration: true,
-      contextIsolation: false,
-    },
-  })
+    const childWindow = new BrowserWindow({
+        webPreferences: {
+            preload,
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+    })
 
-  if (VITE_DEV_SERVER_URL) {
-    childWindow.loadURL(`${VITE_DEV_SERVER_URL}#${arg}`)
-  } else {
-    childWindow.loadFile(indexHtml, { hash: arg })
-  }
+    if (VITE_DEV_SERVER_URL) {
+        childWindow.loadURL(`${VITE_DEV_SERVER_URL}#${arg}`)
+    } else {
+        childWindow.loadFile(indexHtml, {hash: arg})
+    }
 })
+const base = "C:\\Program Files (x86)\\Common Files\\Adobe\\CEP\\extensions";
+ipcMain.handle('query', async (event, app: string) => {
+    const files = readdirSync(base);
+    const xmlParser = new Parser();
+    const result = [];
+    for (const file of files) {
+        if (statSync(path.join(base, file)).isDirectory()) {
+            const xmlFile = path.join(base, file, "CSXS", "manifest.xml");
+            const xmlContent = readFileSync(xmlFile, {encoding: "utf-8"});
+            const xmlParserResult = (await xmlParser.parseStringPromise(xmlContent));
+            const version = xmlParserResult["ExtensionManifest"]["$"]["ExtensionBundleVersion"];
+            const adobeAppID = xmlParserResult["ExtensionManifest"]["ExecutionEnvironment"][0]["HostList"][0]["Host"].map((value) => value["$"]["Name"]);
+            result.push({name: file, version, adobeNameID: adobeAppID});
+        }
+    }
+    return result;
+})
+ipcMain.handle("open", (event, file: string) => {
+    shell.openPath(path.join(base, file));
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
